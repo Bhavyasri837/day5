1eb0cea4b19094de4ba6bc6bd151a748ce14c569
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-06 15:05:31 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,20 @@
+/*
+ 1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5
+ */
+public class pattern1 {
+    public static void main(String args[])
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+}    
+}
---
+++ pattern5.java
@@ -0,0 +1,18 @@
+/*A B C D E 
+B C D E   
+C D E     
+D E       
+E      */
+public class pattern5 {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    } 
+}
